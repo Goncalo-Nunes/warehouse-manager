7f8edc485d9aa72f8bc602984f234b55c59a4b12
Miguel Santos <ist152997@tecnico.ulisboa.pt>
2021-11-08 01:05:49 +0000

    Work in progress

--- src/ggc/app/lookups/DoLookupProductBatchesUnderGivenPrice.java
+++ src/ggc/app/lookups/DoLookupProductBatchesUnderGivenPrice.java
@@ -3,6 +3,9 @@ package ggc.app.lookups;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.core.WarehouseManager;
+import ggc.core.Warehouse;
+import ggc.app.exception.UnknownProductKeyException;
+import ggc.core.BatchComparator;
 //FIXME import classes
 
 /**
@@ -13,11 +16,35 @@ public class DoLookupProductBatchesUnderGivenPrice extends Command<WarehouseMana
   public DoLookupProductBatchesUnderGivenPrice(WarehouseManager receiver) {
     super(Label.PRODUCTS_UNDER_PRICE, receiver);
     //FIXME add command fields
+    addStringField("id", Message.requestProductKey());
   }
 
   @Override
   public void execute() throws CommandException {
     //FIXME implement command
+   /* try {
+      _display.popup(_receiver.requestPriceLimit());
+      
+      _display.addLine(_receiver.getBatches());
+      _display.display();
+    } 
+      catch (UnknownProductKeyException e) {
+      throw new UnknownProductKeyException(e.getProductKey());
+      */
+      try {
+        ArrayList<Lookup> lookup = new ArrayList<>();
+        for(Product product : getProducts()) {
+          for(Batch batch : product.getBatches()) {
+            if (_receiver.requestPriceLimit() > batch.getPrice())
+            lookup.add(batch);
+        _display.popup(_receiver.lookup);
+        
+
+      }
+    }
+      }
+      catch (UnknownProductKeyException e) {
+          throw new UnknownProductKeyException(e.getProductKey());}
   }
 
 }
