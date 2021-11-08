65e25b8955468bf95523080f51b47a3018f108e5
Miguel Santos <ist152997@tecnico.ulisboa.pt>
2021-11-08 01:06:48 +0000

    Work in progress

--- src/ggc/app/lookups/DoLookupPaymentsByPartner.java
+++ src/ggc/app/lookups/DoLookupPaymentsByPartner.java
@@ -3,6 +3,7 @@ package ggc.app.lookups;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.core.WarehouseManager;
+import ggc.core.exception.UnknownPartnerException;
 //FIXME import classes
 
 /**
@@ -13,11 +14,18 @@ public class DoLookupPaymentsByPartner extends Command<WarehouseManager> {
   public DoLookupPaymentsByPartner(WarehouseManager receiver) {
     super(Label.PAID_BY_PARTNER, receiver);
     //FIXME add command fields
+
+    addStringField("id", Message.requestPartnerKey());
   }
 
   @Override
   public void execute() throws CommandException {
     //FIXME implement command
+    String id = stringField("id");
+    try {
+      _display.popup(_receiver.getPaymentsFromPartner(id));
+    } catch (UnknownPartnerException e) {
+      throw new UnknownPartnerKeyException(e.getPartnerKey());
   }
 
 }
--- src/ggc/app/partners/DoToggleProductNotifications.java
+++ src/ggc/app/partners/DoToggleProductNotifications.java
@@ -3,6 +3,7 @@ package ggc.app.partners;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.core.WarehouseManager;
+import ggc.app.exception.UnknownProductKeyException;
 //FIXME import classes
 
 /**
@@ -13,11 +14,23 @@ class DoToggleProductNotifications extends Command<WarehouseManager> {
   DoToggleProductNotifications(WarehouseManager receiver) {
     super(Label.TOGGLE_PRODUCT_NOTIFICATIONS, receiver);
     //FIXME add command fields
+    addStringField("id", Message.requestProductKey());
+    addStringField("key", Message.requestPartnerKey());
+
   }
 
   @Override
   public void execute() throws CommandException {
-    //FIXME implement command
+    String key = stringField("key");
+    String id = stringField("id");
+    try {
+      for(Partner parner : getNotifications()) {
+        if (_receiver.getPartnerNotifications(id) = false) {
+
+      }
+    } 
+      catch (UnknownProductKeyException e) {
+      throw new UnknownProductKeyException(e.getProductKey());
   }
 
 }
